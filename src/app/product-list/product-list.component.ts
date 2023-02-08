e2589e83c7a6c1352c6e49868cd52548c26837b4
gokulsuresh10 <104788547+gokulsuresh10@users.noreply.github.com>
2023-02-08 11:14:19 +0530

    1 file modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -5,17 +5,15 @@ import { products } from '../products';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
-  console.log(Profducts);
   products = products;
 
   share() {
     window.alert('The product has been shared!');
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
